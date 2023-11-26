ee65b5309357438d40d8d583c39650a886d2bd86
Kirill Zhukov <siper13@gmail.com>
2023-11-26 13:39:46 +0300

    add logging settings (#25)

--- src/main/kotlin/ru/stersh/bookcrawler/DatabaseFactory.kt
+++ src/main/kotlin/ru/stersh/bookcrawler/DatabaseFactory.kt
@@ -1,12 +1,25 @@
 package ru.stersh.bookcrawler
 
-import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.statements.StatementContext
 
 object DatabaseFactory {
     fun init() {
         Database.connect(
             url = "jdbc:h2:file:$CONFIG_FOLDER/book_crawler",
-            driver = "org.h2.Driver"
+            driver = "org.h2.Driver",
+            databaseConfig = DatabaseConfig {
+                sqlLogger = if (Properties.get("logging.db").toBoolean()) {
+                    StdOutSqlLogger
+                } else {
+                    EmptyDbLogger
+                }
+            }
         )
+    }
+
+    private val EmptyDbLogger = object : SqlLogger {
+        override fun log(context: StatementContext, transaction: Transaction) {
+        }
     }
 }
